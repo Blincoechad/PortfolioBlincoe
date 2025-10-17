ba3269651e6db3e746cf2ad8bd4faf403ded3953
Blincoechad <blincoechad+github@gmail.com>
2025-10-17 15:29:24 -0400

    my programs

---
+++ MyPrograms/VideoFeed/script.js
@@ -0,0 +1,14 @@
+  const cameraFeed = document.getElementById('camerafeed');
+  // uses camo studio app to access phone camera on website
+  navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "user" } // front camera
+  })
+  .then(stream => {
+    cameraFeed.srcObject = stream;
+  })
+  .catch(err => {
+    console.error("Error accessing camera: ", err);
+  });
+
+
+
