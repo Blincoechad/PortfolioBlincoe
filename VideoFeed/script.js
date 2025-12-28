7297b1edfcd8036890574d8ef0c4fe85d9dc6572
blincoechad <blincoechad@gmail.com>
2025-12-27 20:25:59 -0500

    enlarged picture window/ added styling

--- VideoFeed/script.js
+++ VideoFeed/script.js
@@ -1,7 +1,7 @@
   const cameraFeed = document.getElementById('camerafeed');
   // uses camo studio app to access phone camera on website
   navigator.mediaDevices.getUserMedia({
-    video: { facingMode: "user" } // front camera
+    video: { facingMode: "enviroment" } // front camera
   })
   .then(stream => {
     cameraFeed.srcObject = stream;
